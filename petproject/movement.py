2f7c2b4287aadccf9c8c8c5ecbb8ebc48aa93ed7
Jesse van Mourik <jessed.vanmourik@gmail.com>
2022-10-15 00:50:21 +0200

    Unanimated movement to the right until finish is reached.

--- petproject/movement.py
+++ petproject/movement.py
@@ -1,18 +1,25 @@
 from flask import Blueprint, render_template, request
 from board import board_from_form
-
+import time
 bp = Blueprint('movement', __name__)
 
 
-
-
 @bp.route('/right', methods=('POST',))
-def step_right():
+def right_until_stopped():
     board = board_from_form(request.form)
+
     for col in board.board:
         for cell in col:
-            if cell.comment == "start":
-                board.board[cell.y][cell.x + 1].comment = "visited"
-                print()
+            if cell.comment == "end":
+                return render_template('board.html', board=board.board)
+            if cell.comment == "start" or cell.comment == "visited":
+                board = step_right(cell.x, cell.y, board)
 
     return render_template('board.html', board=board.board)
+
+
+def step_right(original_x, original_y, board):
+    if board.board[original_y][original_x + 1].comment == "end":
+        return board
+    board.board[original_y][original_x + 1].comment = "visited"
+    return board
