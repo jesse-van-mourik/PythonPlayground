61d40a5d4effe30561264618ee345de564d5bf8e
Jesse van Mourik <jessed.vanmourik@gmail.com>
2022-11-17 22:38:58 +0100

    Restored petproject folder.

--- application.py
+++ application.py
@@ -4,16 +4,12 @@ import dijkstra
 import astar
 
 
-def create_app():
-    app = Flask(__name__)
-    app.config['SECRET_KEY'] = '12345'
+application = Flask(__name__)
+application.config['SECRET_KEY'] = '12345'
 
-    app.register_blueprint(main.application)
-    app.register_blueprint(dijkstra.application)
-    app.register_blueprint(astar.application)
-
-    return app
-
+application.register_blueprint(main.application)
+application.register_blueprint(dijkstra.application)
+application.register_blueprint(astar.application)
 
 if __name__ == "__main__":
-    create_app().run()
+    application.run()
