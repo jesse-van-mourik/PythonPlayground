277650c1b76d4b0f3389d3e67a8fde886eeca78a
Jesse van Mourik <jessed.vanmourik@gmail.com>
2022-11-17 21:12:44 +0100

    Renamed __init__ to application. Renamed application to general.

--- petproject/application.py
+++ petproject/application.py
@@ -1,55 +1,28 @@
-from flask import Blueprint, render_template, request, make_response, url_for, redirect, Flask
-from board import Board
+import os
+from flask import Flask, render_template
+from petproject import dijkstra, astar, general
 
 
-application = Blueprint('main', __name__)
-
-
-@application.route('/')
-def index():
-    application = app = Flask(__name__, instance_relative_config=True)
+def create_app(test_config=None):
+    app = Flask(__name__, instance_relative_config=True)
     app.config['SECRET_KEY'] = '12345'
-    from petproject import application
-    app.register_blueprint(application.application)
 
-    # from petproject.Excluded import movement
-    # app.register_blueprint(movement.application)
+    if test_config is None:
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        app.config.from_pyfile(test_config)
 
-    from petproject import dijkstra
+    # ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    app.register_blueprint(general.application)
     app.register_blueprint(dijkstra.application)
-
-    from petproject import astar
     app.register_blueprint(astar.application)
 
-    return render_template('home.html')
+    return app
 
 
-@application.route('/cellform')
-def display_cell_form():
-    return render_template('cellform.html')
-
-
-@application.route('/board', methods=('POST',))
-def create_board():
-    board = Board()
-    for i in range(1, 11):
-        key_x = 'wall' + str(i) + 'x'
-        key_y = 'wall' + str(i) + 'y'
-        wall_x = int(request.form[key_x]) - 1
-        wall_y = int(request.form[key_y]) - 1
-        board.board[wall_y][wall_x].comment = 'wall'
-
-    startx = int(request.form['startx']) - 1
-    starty = int(request.form['starty']) - 1
-    board.board[starty][startx].comment = 'start'
-
-    endx = int(request.form['endx']) - 1
-    endy = int(request.form['endy']) - 1
-    board.board[endy][endx].comment = 'end'
-
-    return render_template('board.html', board=board.board)
-
-
-
-
-
+create_app().run()
---
+++ petproject/general.py
@@ -0,0 +1,40 @@
+from flask import Blueprint, render_template
+from board import Board
+
+application = Blueprint('general', __name__)
+
+
+@application.route('/')
+def index():
+    return render_template('home.html')
+
+
+@application.route('/cellform')
+def display_cell_form():
+    return render_template('cellform.html')
+
+
+@application.route('/board', methods=('POST',))
+def create_board():
+    board = Board()
+    for i in range(1, 11):
+        key_x = 'wall' + str(i) + 'x'
+        key_y = 'wall' + str(i) + 'y'
+        wall_x = int(request.form[key_x]) - 1
+        wall_y = int(request.form[key_y]) - 1
+        board.board[wall_y][wall_x].comment = 'wall'
+
+    startx = int(request.form['startx']) - 1
+    starty = int(request.form['starty']) - 1
+    board.board[starty][startx].comment = 'start'
+
+    endx = int(request.form['endx']) - 1
+    endy = int(request.form['endy']) - 1
+    board.board[endy][endx].comment = 'end'
+
+    return render_template('board.html', board=board.board)
+
+
+
+
+
