0e1e6ad25fc085087905119e780a020a63e1e0dc
Jesse van Mourik <jessed.vanmourik@gmail.com>
2022-11-17 21:53:58 +0100

    Renamed app to application

--- petproject/application.py
+++ petproject/application.py
@@ -4,25 +4,25 @@ from petproject import dijkstra, astar, general
 
 
 def create_app(test_config=None):
-    app = Flask(__name__, )
-    app.config['SECRET_KEY'] = '12345'
+    application = Flask(__name__, )
+    application.config['SECRET_KEY'] = '12345'
 
     # if test_config is None:
-    #     app.config.from_pyfile('config.py', silent=True)
+    #     application.config.from_pyfile('config.py', silent=True)
     # else:
-    #     app.config.from_pyfile(test_config)
+    #     application.config.from_pyfile(test_config)
     #
     # # ensure the instance folder exists
     # try:
-    #     os.makedirs(app.instance_path)
+    #     os.makedirs(application.instance_path)
     # except OSError:
     #     pass
 
-    app.register_blueprint(general.application)
-    app.register_blueprint(dijkstra.application)
-    app.register_blueprint(astar.application)
+    application.register_blueprint(general.application)
+    application.register_blueprint(dijkstra.application)
+    application.register_blueprint(astar.application)
 
-    return app
+    return application
 
 
 if __name__ == "__main__":
