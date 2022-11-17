3c063399cac69b1f0e97d8858e6880017a44bf2e
Jesse van Mourik <jessed.vanmourik@gmail.com>
2022-11-17 21:23:26 +0100

    Comment out redundant (?) code.

--- petproject/application.py
+++ petproject/application.py
@@ -4,19 +4,19 @@ from petproject import dijkstra, astar, general
 
 
 def create_app(test_config=None):
-    app = Flask(__name__, instance_relative_config=True)
+    app = Flask(__name__, )
     app.config['SECRET_KEY'] = '12345'
 
-    if test_config is None:
-        app.config.from_pyfile('config.py', silent=True)
-    else:
-        app.config.from_pyfile(test_config)
-
-    # ensure the instance folder exists
-    try:
-        os.makedirs(app.instance_path)
-    except OSError:
-        pass
+    # if test_config is None:
+    #     app.config.from_pyfile('config.py', silent=True)
+    # else:
+    #     app.config.from_pyfile(test_config)
+    #
+    # # ensure the instance folder exists
+    # try:
+    #     os.makedirs(app.instance_path)
+    # except OSError:
+    #     pass
 
     app.register_blueprint(general.application)
     app.register_blueprint(dijkstra.application)
