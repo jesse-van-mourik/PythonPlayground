e7ee0b338f02271de38341fcfd30ef39bb59ebd7
Jesse van Mourik <jessed.vanmourik@gmail.com>
2023-02-18 10:18:24 +0100

    Display plot on stats page

--- application.py
+++ application.py
@@ -4,6 +4,7 @@ import contact
 import main
 import dijkstra
 import astar
+import stats
 
 application = Flask(__name__)
 application.config['SECRET_KEY'] = '12345'
@@ -12,6 +13,7 @@ application.register_blueprint(main.application)
 application.register_blueprint(dijkstra.application)
 application.register_blueprint(astar.application)
 application.register_blueprint(contact.application)
+application.register_blueprint(stats.application)
 
 if __name__ == "__main__":
     application.run()
--- stats.py
+++ stats.py
@@ -1,17 +1,40 @@
+import base64
+import io
 import cbsodata
+from matplotlib import pyplot as plt
+from matplotlib.backends.backend_agg import FigureCanvasAgg
+from flask import Blueprint, Response
 
-table_id = '85058NED'
-year = 2015
-province = 'PV25  '
+# table_id = '85058NED'
+# year = 2015
+# province = 'PV25  '
+#
+# ds = cbsodata.get_data(
+#       table_id=table_id,
+#       filters="Perioden gt '" + str(year) + "JJ00' "
+#               "and SoortBewoondeWoonruimten eq 'A050218' "
+#               "and RegioS eq '" + province + "'",
+#       select='SoortBewoondeWoonruimten, '
+#              'Perioden, '
+#              'BewoondeWoonruimte_1, '
+#              'RegioS'
+# )
+# print(ds)
+from matplotlib.backends.backend_template import FigureCanvas
+from matplotlib.figure import Figure
 
-ds = cbsodata.get_data(
-      table_id=table_id,
-      filters="Perioden gt '" + str(year) + "JJ00' " 
-              "and SoortBewoondeWoonruimten eq 'A050218' "
-              "and RegioS eq '" + province + "'",
-      select='SoortBewoondeWoonruimten, '
-             'Perioden, '
-             'BewoondeWoonruimte_1, '
-             'RegioS'
-)
-print(ds)
+application = Blueprint('stats', __name__)
+
+
+@application.route('/plot.png')
+def build_plot():
+    y = [1, 2, 3, 4, 5]
+    x = [2, 4, 6, 8, 10]
+
+    fig = Figure()
+    plot = fig.add_subplot(1, 1, 1)
+    plot.plot(x, y)
+    img = io.BytesIO()
+
+    FigureCanvasAgg(fig).print_png(img)
+    return Response(img.getvalue(), mimetype='image/png')
